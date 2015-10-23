a6867c62b78b77e9829e7e025f4899989649a16f
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2015-10-23 00:07:57 -0200

    arudino code

--- arduino/read_piezometers/read_piezometers.ino
+++ arduino/read_piezometers/read_piezometers.ino
@@ -1,65 +1,61 @@
-//Number of input pins
-const int IN_PINS = 1;
-//Number of output pins
-const int OUT_PINS = 2;
-//Delay between reads in ms
-const int DELAY = 10;
-//Needed reads
-const int N_READS = 10;
-
-//First analogic input pin used
-const int INITIAL_IN_PIN = A0;
-//which output pin is the least signficative
-const int INITIAL_OUT_PIN = 3;
-//Min analog value
-const int MIN_VALUE = 0;
-//Max analog value
-const int MAX_VALUE = IN_PINS*1023;
-const int STEP = (MAX_VALUE - MIN_VALUE) / (2 << OUT_PINS);
+const int piezometer0 = A0, piezometer1 = A1;
+const int READS = 50;
+const int N_FRACO = 4;
+const int N_MEDIO = 4;
+const int N_FORTE = 3;
+const int OUT_0 = 3, OUT_1 = 4;
 
 void setup() {
   //Set piezometers as pullup input
-  //pinMode(piezometer0, INPUT);
-  //pinMode(piezometer1, INPUT);
-  
+  analogReference(INTERNAL);
   //Start the serial
   Serial.begin(9600);
 
   //Set digital output (2 pins)
-  for (int i = 0; i < OUT_PINS; i++)
-    pinMode(INITIAL_OUT_PIN + i, OUTPUT);
+  pinMode(OUT_0, OUTPUT);
+  pinMode(OUT_1, OUTPUT);
 
 }
 
 int out_val = 0x00;
+int real_val = 0;
 int count = 0;
+int count_fraco = 0, count_medio = 0, count_forte = 0;
+int estado = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
-  int val = 0;
-  int digital_val = 0;
-  for (int i = 0; i < IN_PINS; i++)
-    val += analogRead(INITIAL_IN_PIN + i);
-  
-  for (digital_val = 0; digital_val < (2 << OUT_PINS); digital_val++)
-    if ((digital_val == 0 && val <= STEP) 
-        || (val >= STEP * digital_val && val <= (STEP * (digital_val + 1)))
-        || val >= (STEP * (digital_val + 1)))
-        break;
-  if (digital_val == out_val || count == 0) {
-    count++;
-    out_val = digital_val;
-    if (count == N_READS) {
-      count = 0;
-      for (int i = 0; i < OUT_PINS; i++) {
-        digitalWrite(INITIAL_OUT_PIN + i, out_val & (1 << i));
-      }
+  int val = analogRead(piezometer0);/* + analogRead(piezometer1);*/
+  int out = 0;
+  if (val >= 400)
+    out++;
+  if (val >= 700)
+    out++;
+  if (val >= 900)
+    out++;
+ 
+ /* Serial.println(val);*/
+ if (val >= 4) {
+    count_fraco++;
+    if (val >= 9)
+      count_medio++;
+ }
+ else {
+   /* Serial.println("Sem apertar");*/
+ }
+ count++;
+
+ if (count == READS) {
+    if (count_fraco >= N_FRACO) {
+       if (count_medio >= N_MEDIO)
+          Serial.println("Medio apertado");
+       else
+          Serial.println("Pouco Apertado");
     }
-  }
-  else {
-    count = 0;
-    out_val = digital_val;
-  }
-  Serial.println(val);
-  
-  delay(DELAY);
+   
+    else
+      Serial.println("NAO APERTADO");
+    count = count_fraco = count_medio = count_forte = 0;
+ }
+  delay(10);
 }
